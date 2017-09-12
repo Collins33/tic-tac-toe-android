09e0ac9bc506389925800d3630826f4e0dab8901
collinsnjau39@gmail.com <collinsnjau39@gmail.com>
2017-09-12 23:04:51 +0300

    I have added a drop in method that drops the yellow chip when the view is clicked

--- app/src/main/java/com/example/root/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/root/tictactoe/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.root.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dropIn(View view){
+
+        ImageView counter=(ImageView) view;
+
+        counter.setTranslationY(-1000f);
+
+        counter.setImageResource(R.drawable.yellow);
+        //animate back to the screen
+        counter.animate().translationYBy(1000f).setDuration(300);
     }
 }
